9943a79e7ef424881a327cd698952f91fff41fc3
Arnaldo Escossio <arnaldoescossio@gmail.com>
2026-03-05 20:31:50 -0500

    Improve message to handle expired token

--- week_2/day_2/interfaces/api/security/security.py
+++ week_2/day_2/interfaces/api/security/security.py
@@ -1,4 +1,4 @@
-from jose import JWTError, jwt
+from jose import JWTError, jwt, ExpiredSignatureError
 from fastapi import HTTPException, Depends
 from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
 
@@ -15,5 +15,7 @@ def verify_token(credentials: HTTPAuthorizationCredentials = Depends(security)):
     try:
         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
         return payload
+    except ExpiredSignatureError:
+        raise HTTPException(status_code=401, detail="Token has expired")
     except JWTError:
-        raise HTTPException(status_code=401, detail="Invalid or expired token")
+        raise HTTPException(status_code=401, detail="Invalid token")
